0617af07c7d4a5c3708ea514c6775ea0e65cbb71
Shodai Yada <sho8.kthy@gmail.com>
2022-10-12 13:37:49 +0900

    first commit

---
+++ IrisEyeGlance/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IrisEyeGlance
+//
+//  Created by 矢田翔大 on 2022/10/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
