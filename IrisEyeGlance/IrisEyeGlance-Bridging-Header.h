d08cf7efe5908a34d8561b6bc357327d86040c07
Shodai Yada <sho8.kthy@gmail.com>
2022-10-23 01:26:47 +0900

    add framework of face mesh

---
+++ IrisEyeGlance/IrisEyeGlance-Bridging-Header.h
@@ -0,0 +1,7 @@
+//
+//  IrisEyeGlance-Bridging-Header.h
+//  IrisEyeGlance
+//
+//  Created by 矢田翔大 on 2022/10/23.
+//
+#import <SYFaceMesh/SYFaceMesh.h>
--- IrisEyeGlance/ViewController.swift
+++ IrisEyeGlance/ViewController.swift
@@ -6,14 +6,32 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate, TrackerDelegate {
+    @IBOutlet weak var imageview: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    
+    let camera = Camera()
+//    let tracker: SYFaceMesh = SYFaceMesh()!
+    var frameNum: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        camera.setSampleBufferDelegate(self)
+        camera.start()
     }
-
-
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer)
+        
+        DispatchQueue.main.async {
+            self.imageview.image = UIImage(ciImage: CIImage(cvPixelBuffer: pixelBuffer!))
+        }
+        frameNum += 1
+        label.text = "\(frameNum)"
+    }
+    
+    
 }
 
