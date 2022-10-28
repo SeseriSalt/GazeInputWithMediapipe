d708133aca449f4612aa04526a75278bb42b0f03
Shodai Yada <sho8.kthy@gmail.com>
2022-10-29 03:27:31 +0900

    get iris to depth.

--- IrisEyeGlance/ViewController.swift
+++ IrisEyeGlance/ViewController.swift
@@ -13,10 +13,16 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
     
     @IBOutlet weak var imageview: UIImageView!
     @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var leftLavel: UILabel!
+    @IBOutlet weak var rightLabel: UILabel!
     
     let camera = Camera()
     let tracker: SYIris = SYIris()!
-    var frameNum: Int = 0
+    
+    public let FOCAL_LENGTH: Float = 1304.924438
+    public let WIDTH: Float = 1080.0
+    public let HEIGHT: Float = 1920.0
+    public var frameNum: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,26 +36,55 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
         let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer)
         tracker.processVideoFrame(pixelBuffer)
         
+        frameNum += 1
         DispatchQueue.main.async {
             self.imageview.image = UIImage(ciImage: CIImage(cvPixelBuffer: pixelBuffer!))
+            self.label.text = "\(self.frameNum)"
         }
-        frameNum += 1
-        label.text = "\(frameNum)"
     }
     
     func irisTracker(_ irisTracker: SYIris!, didOutputLandmarks landmarks: [Landmark]!) {
-        print("!!!!!!!!!!!!\(frameNum)!!!!!!!!!!!!!")
+        var landmarkAll : [[Float]] = []
         if let unwrapped = landmarks {
-            for (i, point) in unwrapped.enumerated() {
-                print("\(i): \(point.x), \(point.y), \(point.z)")
+            for (point) in unwrapped {
+                landmarkAll.append([point.x, point.y, point.z])
             }
+            let leftEyeLandmark = [
+                landmarkAll[468],
+                landmarkAll[469],
+                landmarkAll[470],
+                landmarkAll[471],
+                landmarkAll[472],
+            ]
+            let rightEyeLandmark = [
+                landmarkAll[473],
+                landmarkAll[474],
+                landmarkAll[475],
+                landmarkAll[476],
+                landmarkAll[477],
+            ]
+            
+            //Depthの計算
+            let leftIrisSize = caluclateIrisDiamater(landmark: leftEyeLandmark, imageSize: [WIDTH, HEIGHT])
+            let rightIrisSize = caluclateIrisDiamater(landmark: rightEyeLandmark, imageSize: [WIDTH, HEIGHT])
+            
+            let leftDepth_mm = caluclateDepth(centerPoint: leftEyeLandmark[0] , focalLength: FOCAL_LENGTH, irisSize: leftIrisSize, width: WIDTH, height: HEIGHT)
+            let rightDepth_mm = caluclateDepth(centerPoint: rightEyeLandmark[0], focalLength: FOCAL_LENGTH, irisSize: rightIrisSize, width: WIDTH, height: HEIGHT)
+            
+            let leftDepth = Int(round(leftDepth_mm / 10))
+            let rightDepth = Int(round(rightDepth_mm / 10))
+            
+            DispatchQueue.main.async {
+                self.leftLavel.text = "\(leftDepth)"
+                self.rightLabel.text = "\(rightDepth)"
+            }
+            
         }
     }
     
     func irisTracker(_ irisTracker: SYIris!, didOutputPixelBuffer pixelBuffer: CVPixelBuffer!) {
         DispatchQueue.main.async {
             self.imageview.image = UIImage(ciImage: CIImage(cvPixelBuffer: pixelBuffer))
-            
         }
     }
 }
